8ed4c2b3f07772b71eff9f7db5fd16fd5101ff83
Nikhil Buduma <nbuduma@sumologic.com>
2015-06-27 11:10:42 -0700

    uploader

--- lambda_uploader/uploader.py
+++ lambda_uploader/uploader.py
@@ -1,15 +1,28 @@
-from zipfile_infolist import print_info
 import zipfile
+import datetime
 
-print 'creating archive'
+def print_info(archive_name):
+    zf = zipfile.ZipFile(archive_name)
+    for info in zf.infolist():
+        print info.filename
+        print '\tComment:\t', info.comment
+        print '\tModified:\t', datetime.datetime(*info.date_time)
+        print '\tSystem:\t\t', info.create_system, '(0 = Windows, 3 = Unix)'
+        print '\tZIP version:\t', info.create_version
+        print '\tCompressed:\t', info.compress_size, 'bytes'
+        print '\tUncompressed:\t', info.file_size, 'bytes'
+        print
+
+print
+print 'CREATING ARCHIVE'
 zf = zipfile.ZipFile('lambda_zip.zip', mode='w')
 try:
-    print 'adding lambda.js'
+    print 'ADDING LAMBDA.JS'
     zf.write('lambda.js')
-    print 'adding card_functions.js'
+    print 'ADDING CARD_FUNCTIONS.JS'
     zf.write('card_functions.js')
 finally:
-    print 'closing'
+    print 'CLOSING ARCHIVE'
     zf.close()
 
 print
