1bf29232a7278158c80b0153a6f1f1fe2f9a5107
Simon Last <hello@simonlast.org>
2015-06-27 12:46:01 -0700

    Read from firebase.

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -1,19 +1,26 @@
+var Reflux          = require("reflux");
 var React           = require("react/addons");
 var classSet        = React.addons.classSet;
 var PureRenderMixin = React.addons.PureRenderMixin;
 var GridLayer       = require("../GridLayer/GridLayer.jsx")
 var LinkLayer       = require("../LinkLayer/LinkLayer.jsx")
 var CardLayer       = require("../CardLayer/CardLayer.jsx")
+var GraphStore      = require("../../stores/GraphStore.js");
 
 
 var App = React.createClass({
 
+	mixins: [
+		PureRenderMixin,
+		Reflux.connect(GraphStore, "graph")
+	],
+
 	render: function() {
 		return (
 			<div className="App">
-				<GridLayer />
-				<LinkLayer />
-				<CardLayer />
+				<GridLayer graph={this.state.graph}/>
+				<LinkLayer graph={this.state.graph}/>
+				<CardLayer graph={this.state.graph}/>
 			</div>
 		);
 	}
