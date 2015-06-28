94241c29fa7979c7600b6f8f261fa1b3cb40d1c6
Simon Last <hello@simonlast.org>
2015-06-27 21:40:49 -0700

    Refactor grid helpers.

--- app/components/CardLayer/CardLayer.jsx
+++ app/components/CardLayer/CardLayer.jsx
@@ -5,6 +5,7 @@ var PureRenderMixin  = React.addons.PureRenderMixin;
 var Card             = require("../Card/Card.jsx");
 var EmptyCell        = require("../EmptyCell/EmptyCell.jsx");
 var selectionActions = require("../../actions/selectionActions.js");
+var gridHelpers      = require("../../helpers/gridHelpers.js");
 
 
 var CardLayer = React.createClass({
@@ -14,10 +15,10 @@ var CardLayer = React.createClass({
 
 	render: function() {
 		var _this = this;
-		var cellsInView = this.getCellsInView();
+		var cellsInView = gridHelpers.getCellsInView(this.props.grid);
 
 		if (this.props.drag.get("cardId")) {
-			var dragCellCoordinates = this.closestCellToDrag(cellsInView);
+			var dragCellCoordinates = gridHelpers.closestCellToDrag(cellsInView, this.props.grid, this.props.drag);
 		}
 
 		var cells = _.map(cellsInView, function(coordinate) {
@@ -34,44 +35,12 @@ var CardLayer = React.createClass({
 	},
 
 
-	getCellsInView: function() {
-		var grid         = this.props.grid;
-		var windowHeight = grid.get("windowHeight");
-		var windowWidth  = grid.get("windowWidth");
-		var cellWidth    = grid.get("cellWidth");
-		var transX       = grid.get("transX");
-		var transY       = grid.get("transY");
-
-		var topXIndex = Math.floor(transX / cellWidth);
-		var topYIndex = Math.floor(transY / cellWidth);
-
-		var xStart = (cellWidth - transY) % cellWidth;
-		var yStart = (cellWidth - transX) % cellWidth;
-
-		var numX = Math.ceil((windowWidth + xStart) / cellWidth);
-		var numY = Math.ceil((windowHeight + yStart) / cellWidth);
-
-		var coordinates = [];
-
-		for(var x = 0; x < numX; x++) {
-			for(var y = 0; y < numY; y++) {
-				coordinates.push({
-					x: topXIndex + x,
-					y: topYIndex + y
-				});
-			}
-		}
-
-		return coordinates;
-	},
-
-
 	renderCell: function(coordinate, dragCellCoordinates) {
 		var cellWidth  = this.props.grid.get("cellWidth");
 		var x          = (coordinate.x * cellWidth) - this.props.grid.get("transX");
 		var y          = (coordinate.y * cellWidth) - this.props.grid.get("transY");
 
-		var cardIdForCoordinate = this.getCardIdForCoordinate(coordinate);
+		var cardIdForCoordinate = gridHelpers.getCardIdForCoordinate(coordinate, this.props.graph.cards);
 
 		var isDraggingCard = false
 		if (cardIdForCoordinate) {
@@ -112,45 +81,6 @@ var CardLayer = React.createClass({
 	},
 
 
-	getCardIdForCoordinate: function(coordinate) {
-		var cardIdForCoordinate = null;
-		_.find(this.props.graph.cards, function(cardData, cardId) {
-			if (cardData.x === coordinate.x && cardData.y === coordinate.y) {
-				cardIdForCoordinate = cardId;
-				return true;
-			}
-			else {
-				return false;
-			}
-		});
-		return cardIdForCoordinate;
-	},
-
-
-	closestCellToDrag: function(cellsInView) {
-		var grid         = this.props.grid;
-		var windowHeight = grid.get("windowHeight");
-		var windowWidth  = grid.get("windowWidth");
-		var cellWidth    = grid.get("cellWidth");
-		var transX       = grid.get("transX");
-		var transY       = grid.get("transY");
-
-		var dragX = this.props.drag.get("startX") + this.props.drag.get("offsetX");
-		var dragY = this.props.drag.get("startY") + this.props.drag.get("offsetY");
-
-		var offsetDragX = dragX + transX;
-		var offsetDragY = dragY + transY;
-
-		var cellX = Math.round(offsetDragX / cellWidth);
-		var cellY = Math.round(offsetDragY / cellWidth);
-
-		return {
-			x: cellX,
-			y: cellY
-		};
-	},
-
 });
-
 
 module.exports = CardLayer;
---
+++ app/helpers/gridHelpers.js
@@ -0,0 +1,76 @@
+var _ = require("lodash");
+
+
+var gridHelpers = {};
+
+
+gridHelpers.getCellsInView = function(grid) {
+	var windowHeight = grid.get("windowHeight");
+	var windowWidth  = grid.get("windowWidth");
+	var cellWidth    = grid.get("cellWidth");
+	var transX       = grid.get("transX");
+	var transY       = grid.get("transY");
+
+	var topXIndex = Math.floor(transX / cellWidth);
+	var topYIndex = Math.floor(transY / cellWidth);
+
+	var xStart = (cellWidth - transY) % cellWidth;
+	var yStart = (cellWidth - transX) % cellWidth;
+
+	var numX = Math.ceil((windowWidth + xStart) / cellWidth);
+	var numY = Math.ceil((windowHeight + yStart) / cellWidth);
+
+	var coordinates = [];
+
+	for(var x = 0; x < numX; x++) {
+		for(var y = 0; y < numY; y++) {
+			coordinates.push({
+				x: topXIndex + x,
+				y: topYIndex + y
+			});
+		}
+	}
+
+	return coordinates;
+};
+
+
+gridHelpers.getCardIdForCoordinate = function(coordinate, cards) {
+	var cardIdForCoordinate = null;
+	_.find(cards, function(cardData, cardId) {
+		if (cardData.x === coordinate.x && cardData.y === coordinate.y) {
+			cardIdForCoordinate = cardId;
+			return true;
+		}
+		else {
+			return false;
+		}
+	});
+	return cardIdForCoordinate;
+};
+
+
+gridHelpers.closestCellToDrag = function(cellsInView, grid, drag) {
+	var windowHeight = grid.get("windowHeight");
+	var windowWidth  = grid.get("windowWidth");
+	var cellWidth    = grid.get("cellWidth");
+	var transX       = grid.get("transX");
+	var transY       = grid.get("transY");
+
+	var dragX = drag.get("startX") + drag.get("offsetX");
+	var dragY = drag.get("startY") + drag.get("offsetY");
+
+	var offsetDragX = dragX + transX;
+	var offsetDragY = dragY + transY;
+
+	var cellX = Math.round(offsetDragX / cellWidth);
+	var cellY = Math.round(offsetDragY / cellWidth);
+
+	return {
+		x: cellX,
+		y: cellY
+	};
+};
+
+
+module.exports = gridHelpers;
