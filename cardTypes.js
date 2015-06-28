d0a31f565c183a9ae9c753fa12d528e7148fdcdd
Nikhil Buduma <nbuduma@sumologic.com>
2015-06-27 21:22:49 -0700

    port 80 opened and cardtypes

--- cardTypes.js
+++ cardTypes.js
@@ -6,10 +6,10 @@ var cardTypes = {};
 
 cardTypes.number = {
 	humanReadableName: "Number",
-	cardClass: "variable",
-	run: function() {
+	cardClass: "Variable",
+	run: function(inputs, cardData, httpData, requestID) {
 		return new Promise(function(resolve, reject) {
-			return resolve(null);
+			return resolve(cardData.value);
 		});
 	},
 	inputs: [],
@@ -20,14 +20,61 @@ cardTypes.number = {
 cardTypes.add = {
 	humanReadableName: "Add",
 	cardClass: "function",
-	run: function() {
+	run: function(inputs, cardData, httpData, requestID) {
 		return new Promise(function(resolve, reject) {
-			return resolve(null);
+			return resolve(inputs["number1"] + inputs["number2"]);
 		});
 	},
 	inputs: ["number1", "number2"],
 	hasOutput: true,
 };
 
+cardTypes.subtract = {
+	humanReadableName: "Subtract",
+	cardClass: "function",
+	run: function(inputs, cardData, httpData, requestID) {
+		return new Promise(function(resolve, reject) {
+			return resolve(inputs["number1"] - inputs["number2"]);
+		});
+	},
+	inputs: ["number1", "number2"],
+	hasOutput: true,
+};
+
+cardTypes.divide = {
+	humanReadableName: "Divide",
+	cardClass: "function",
+	run: function(inputs, cardData, httpData, requestID) {
+		return new Promise(function(resolve, reject) {
+			return resolve(inputs["number1"] / inputs["number2"]);
+		});
+	},
+	inputs: ["number1", "number2"],
+	hasOutput: true,
+};
+
+cardTypes.multiply = {
+	humanReadableName: "Multiply",
+	cardClass: "function",
+	run: function(inputs, cardData, httpData, requestID) {
+		return new Promise(function(resolve, reject) {
+			return resolve(inputs["number1"] * inputs["number2"]);
+		});
+	},
+	inputs: ["number1", "number2"],
+	hasOutput: true,
+};
+
+cardTypes.sort = {
+	humanReadableName: "SortBy",
+	cardClass: "function",
+	run: function(inputs, cardData, httpData, requestID) {
+		return new Promise(function(resolve, reject) {
+			return resolve(inputs["number1"] * inputs["number2"]);
+		});
+	},
+	inputs: ["number1", "number2"],
+	hasOutput: true,
+};
 
 module.exports = cardTypes;
