d190dbc84d2a7ed15fdbd2d5883efba8e7ed754a
Nikhil Buduma <nbuduma@sumologic.com>
2015-06-27 17:49:46 -0700

    i hate git

--- Lambda.js
+++ Lambda.js
@@ -1,5 +1,6 @@
 // dependencies
 var AWS = require('aws-sdk');
+var Firebase = require('firebase');
  
 /*
 exports.handler = function(event, context) {
@@ -14,6 +15,9 @@ exports.handler = function(event, context) {
     console.log('Received event:', JSON.stringify(event, null, 2));
     var message = event.Records[0].Sns.Message;
     console.log('From SNS:', message);
-    context.succeed(message);
+    graph_id = message.graphId;
+    new Firebase("https://forge-app2.firebaseio.com/graphs/" + graph_id).once('value', function(snap) {
+		console.log(snap.val());
+	});
 };
 
