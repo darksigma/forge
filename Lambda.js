ad13fcf33c8f1b76f8d76b22d9d1c47905fd6c05
Nikhil Buduma <nbuduma@sumologic.com>
2015-06-27 20:19:10 -0700

    dfs

--- Lambda.js
+++ Lambda.js
@@ -1,6 +1,8 @@
 // dependencies
 var AWS = require('aws-sdk');
 var Firebase = require('firebase');
+var Promise = require('promise');
+var PromiseSeries = require('promise-series');
 var globalConfig = require("./globalConfig");
 var cardTypes = require("./cardTypes")
  
@@ -13,7 +15,27 @@ console.log('Loading function');
 
 */
 
-
+evaluateCard = function(cards, cardId, requestId) {
+    return new Promise(function(resolve, reject) {
+        var typeInfo = cardTypes[cards[cardId].type];
+        var ins = {};
+        var series = new PromiseSeries();
+        for (var inputLabel in typeInfo.inputs){
+            inputCardId = cards[cardId].inputs[inputLabel];
+            inputValue = evaluateCard(cards, inputCardId, requestId);
+            series.add(inputValue);
+        }
+        series.run().then(function(values){
+            var inputs = {};
+            var i;
+            for (i = 0; i < values.length; i++){
+                inputs[typeInfo.inputs[i]] = values[i];
+            }
+            fn = typeInfo.run
+            return resolve(fn(inputs, cards[cardId], requestId))
+        })
+    });
+}
 
 exports.handler = function(event, context) {
     console.log('Received event:', JSON.stringify(event, null, 2));
@@ -31,6 +53,9 @@ exports.handler = function(event, context) {
         console.log("node id: ", message.nodeId);
         var rootNode = cards[message.nodeId];
         console.log("node: ", rootNode);
+
+
+
 		context.succeed(message);
 	});
 };
